ff934caa1e98b4ce88f2e30ea0aeb8fb4b46794d
Ulises <uliamaya13@outlook.com>
2019-03-17 08:26:52 -0600

    version inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    LoginComponent,
+    PagesComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
